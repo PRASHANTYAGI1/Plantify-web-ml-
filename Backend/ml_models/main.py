68773f00e1ac7a37b71d81abd62d679185625e86
PRASHANT TYAGI <prashanttyagi7589@gmail.com>
2025-12-03 17:47:01 +0530

    Update main.py

--- Backend/ml_models/main.py
+++ Backend/ml_models/main.py
@@ -89,6 +89,6 @@ async def predict_potato(image: UploadFile = File(...)):
 
 
 # ------------ START SERVER ------------
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
